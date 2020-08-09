392bc9573c05870b5d3fad917114905f3038ac21
srinuadp <45663074+srinuadp@users.noreply.github.com>
2020-08-09 18:31:44 +0530

    Update website 1.0

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,21 +7,19 @@ exports.handler = async function Index () {
     /**
      * Basic bio
      */
-    fullname: 'Your Name', // ←  Start by adding your name!
+    fullname: 'Durga Prasad', // ←  Start by adding your name!
     title: 'My personal site!',
-    occupation: 'Artist & Photographer',
-    location: 'West Glacier, MT',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
+    occupation: 'Guidewire & Cloud Developer',
+    location: 'Rajahmundry, India',
+    bio: 'Hi 🙋🏻‍♂️, I am Guidewire Policycenter Developer and Aspiring Cloud Developer.',
 
     /**
      * Contact / social
      * - Comment out any item below to remove it from your page
      */
-    email: 'your@email.com',
-    twitter: 'yourTwitter',
-    linkedin: 'your-linkedin-name',
-    instagram: 'yourInsta',
-    facebook: 'your-facebook-name',
+    email: 'Durgaprasad@gmail.com',
+    twitter: '@srinuadp',
+    linkedin: 'Durga Prasad Ambati',
 
     /**
      * Layout
